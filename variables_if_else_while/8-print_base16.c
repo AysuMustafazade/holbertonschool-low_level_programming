597179eddb9f164d67d7c4216ca85336c0808974
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-02 20:42:11 +0000

    Numbers of base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - prints all numbers of base 16 in lowercase
+ * Return: Always 0
+ */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = '0'; c <= '9'; c++)
-    {
-        putchar(c);
-    }
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 
-    for (c = 'a'; c <= 'f'; c++)
-    {
-        putchar(c);
-    }
-
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
