ee1d5e21750096eff5689dbda548b6ff5a08b208
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-02 07:48:53 +0000

    Positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* betty style doc for function main goes there */
+/* 
+ * main - generates a random number and prints whether it is positive, zero, or negative
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
