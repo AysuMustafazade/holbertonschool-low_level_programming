36c8f6c1700f72f109fe62e7afd34db9fe0277a6
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-05 21:55:15 +0000

    Prints putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints _putchar followed by a new line
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
     _putchar('_');
