206f24b29ec3a841319da8baf54cce57059ef3a7
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-06 21:29:04 +0000

    Prints 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
