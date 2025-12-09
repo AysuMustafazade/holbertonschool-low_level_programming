856f95fcad34a43d87c0d935308a031c16323261
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-09 21:09:59 +0000

    Length of string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: pointer to first integer
+ * @b: pointer to second integer
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+        int i;
+
+        i = 0;
+	    while (s[i] != '\0')
+	    {
+	    	i++;
+	    }
+	    return (i);
+}
