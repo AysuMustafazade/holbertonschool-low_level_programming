2c3440c0fae5178bbbbf761199eaf330badbb49f
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-11 08:27:37 +0000

    Reverse string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,21 +4,24 @@
 /**
  * print_rev - prints a string in reverse
  * @s: string
- *
  */
-
+ 
 void print_rev(char *s)
 {
-    int i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    i--;
-    while (i>= 0)
-    {
-        write(1, &s[i], 1);
-        i--;
-    }
-    write(1, "\n", 1);
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	i--;
+
+	while (i >= 0)
+	{
+		write(1, &s[i], 1);
+		i--;
+	}
+
+	write(1, "\n", 1);
 }
