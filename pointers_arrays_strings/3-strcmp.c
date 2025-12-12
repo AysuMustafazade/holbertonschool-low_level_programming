0cc41cba696f055ca13d9649d9796c1f086c70a2
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-12 10:13:49 +0000

    Compare strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,8 @@
  * _strcmp - Compares two strings lexicographically.
  * @s1: The first string to be compared.
  * @s2: The second string to be compared.
- *
+ * Return: 0 if s1 and s2 are equal, a negative integer if s1 is less than s2,
+ * or a positive integer if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
