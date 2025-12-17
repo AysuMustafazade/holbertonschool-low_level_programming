0ffed6792c3021610c0e780852affb4331b0deb5
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-17 21:58:15 +0000

    Factorial of the number

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,15 +2,15 @@
 
 /**
  * factorial - returns the factorial of a given number
- * @n: the given number
+ * @n: the number to calculate factorial
  *
  * Return: factorial of n, or -1 if n < 0
  */
 int factorial(int n)
 {
-    if (n < 0)
-        return -1;
-    if (n == 0)
-        return 1;
-    return n * factorial(n - 1);
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
