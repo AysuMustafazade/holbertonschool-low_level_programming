4c845005f5febf7c84167cea8e059acf622d0287
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-17 21:30:39 +0000

    prints a string using recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * puts_recursion - prints a string followed by a new line using recursion
+* @s: pointer to the string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		write(1, s, 1);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		write(1, "\n", 1);
+	}
+}
