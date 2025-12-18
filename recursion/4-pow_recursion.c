28ad7b2218263f136363dbae1f6636020d4f12b6
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 08:36:34 +0000

    Power of number

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * int _pow_recursion - returns the value of x raised to the power of y
- * @x: the number to be raised
- * @y: the power
+ * @x: based number
+ * @y: exponent
  *
  * Return: the value of x raised to the power of y
  * If y is lower than 0, the function should return -1
@@ -11,8 +11,10 @@
 int _pow_recursion(int x, int y){                                                                           
     if (y < 0){
         return (-1);
-    }else 
+    }else if (y == 0)
     {
-       return  (x**y);
+        return  (1);
+    }else{
+        return (x*_pow_recursion(x, (y-1)));
     }
 }
