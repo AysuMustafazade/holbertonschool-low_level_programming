28512d94d79479dbc4fd907942f2d5d7e4206ac4
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 08:39:26 +0000

    Power of number

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * int _pow_recursion - returns the value of x raised to the power of y
- * @x: based number
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
  * @y: exponent
  *
- * Return: the value of x raised to the power of y
- * If y is lower than 0, the function should return -1
+ * Return: value of x raised to the power of y
+ *         -1 if y is less than 0
  */
 int _pow_recursion(int x, int y)
-{                                                                           
-    if (y < 0){
-        return (-1);
-    }else if (y == 0)
-    {
-        return  (1);
-    }else{
-        return (x*_pow_recursion(x, (y-1)));
-    }
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
