e1b1820af160ec7160e003ad89c79fbd5a1a1f78
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 12:15:43 +0000

    number of arguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[] __attribute__((unused))) {
+int main(int argc, char *argv[] __attribute__((unused))) 
+{
     printf("Total number of arguments: %d\n", argc-1);
 }
