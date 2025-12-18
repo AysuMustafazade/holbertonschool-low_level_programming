f6cd319cc58245d0a93138ab50b1aa89b681a46d
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 12:12:49 +0000

    number of arguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,28 +2,11 @@
 
 /**
  * main - prints the number of arguments
- * @argc: Number of arguments (unused)
+ * @argc: Number of arguments
  * @argv: Array of arguments
  *
  * Return: Always 0
  */
-int main (int argc, char *argv[])
-{
-  int count;
-
-  printf ("This program was called with \"%s\".\n",argv[0]);
-
-  if (argc > 1)
-    {
-      for (count = 1; count < argc; count++)
-	{
-	  printf("argv[%d] = %s\n", count, argv[count]);
-	}
-    }
-  else
-    {
-      printf("The command had no other arguments.\n");
-    }
-
-  return 0;
+int main(int argc, char *argv[]) {
+    printf("Total number of arguments: %d\n", argc);
 }
