095a7b0bd7a96b883e35b2bedee367ac0779ec00
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 12:26:07 +0000

    all arguments it receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[] )
+{
+	int i;
+
+	for (i = 0; argv[i]; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
