3dd5b8804ec7045df4e46aa3b2c0c1e563b78713
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 12:37:57 +0000

    Multipy of two integers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc , char *argv[] __attribute__((unused)))
+{
+	int a, int b;
+
+	if (argc < 2)
+{
+	printf("Error");
+    return (1);
+}   if (argc == 2)
+{
+    printf("%d\n", a*b);
+    return (0);
+}
+}
