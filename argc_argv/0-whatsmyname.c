c0861ff3267f8c1bd8ef1c4c4d54de8c247566b5
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 11:06:43 +0000

    Print program's name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Prints the name of the program
- * @argc: Argument count
- * @argv: Argument vector
+ * @argc: Number of arguments (unused)
+ * @argv: Array of arguments
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-    printf("%s\n", argv[0]);
-    return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
