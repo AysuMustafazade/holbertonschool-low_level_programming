28f70506cfbe6a0e36e1951515204f2d27a925d8
AysuMustafazade <mustafazadays16@gmail.com>
2025-12-18 09:21:47 +0000

    Natural square root of number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
+int _sqrt_helper(int n, int guess);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number
+ * @n: number to find the square root of
  *
- * Return: the natural square root of a number
- * If n does not have a natural square root, the function should return -1
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: base number
- * @y: exponent
- *
- * Return: value of x raised to the power of y
- *         -1 if y is less than 0
+ * Return: natural square root of n, or -1 if none exists
  */
- int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	if (n < 0)
 		return (-1);
 
-	if (y == 0)
-		return (1);
-
-	return (x * _pow_recursion(x, y - 1));
+	return (_sqrt_helper(n, 0));
 }
-int _sqrt_recursion(int n)
-{  
-	if (_pow_recursion(n, 1/2) >= 0)
-	{	return (_pow_recursion(n, 1/2));
 
-    }else 
-	{	return (-1);
-    }
-	return (_pow_recursion(n, 1/2));   
+/**
+ * _sqrt_helper - helper function to find square root
+ * @n: number
+ * @guess: current guess
+ *
+ * Return: square root or -1
+ */
+int _sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+
+	if (guess * guess > n)
+		return (-1);
+
+	return (_sqrt_helper(n, guess + 1));
 }
