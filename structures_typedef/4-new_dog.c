69ef057bea4a3d874a083f4aadc803de383f88dd
AysuMustafazade <mustafazadays16@gmail.com>
2026-01-02 20:45:38 +0000

    Creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,51 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (!s)
+		return (0);
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (!src)
+	{
+		dest[0] = '\0';
+		return (dest);
+	}
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -8,11 +53,12 @@
  * @age: dog's age
  * @owner: dog's owner
  *
- * Return: pointer to new dog, NULL if it fails
+ * Return: pointer to new dog, NULL if fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *n, *o;
 
 	if (!name || !owner)
 		return (NULL);
@@ -21,16 +67,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!d)
 		return (NULL);
 
-	d->name = strdup(name);
-	d->owner = strdup(owner);
-	if (!d->name || !d->owner)
+	n = malloc(_strlen(name) + 1);
+	if (!n)
 	{
-		free(d->name);
-		free(d->owner);
 		free(d);
 		return (NULL);
 	}
 
+	o = malloc(_strlen(owner) + 1);
+	if (!o)
+	{
+		free(n);
+		free(d);
+		return (NULL);
+	}
+
+	_strcpy(n, name);
+	_strcpy(o, owner);
+
+	d->name = n;
 	d->age = age;
+	d->owner = o;
+
 	return (d);
 }
