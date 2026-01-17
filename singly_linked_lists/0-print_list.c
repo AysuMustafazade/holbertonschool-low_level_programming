7112b5a30cce6fd6f2d408a2a6755a3e3e502982
AysuMustafazade <mustafazadays16@gmail.com>
2026-01-17 21:00:33 +0000

    struct

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,13 +7,6 @@
  * Return: number of nodes
  */
 
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-
 size_t print_list(const list_t *h)
 {
     size_t count = 0;
