286bfad50fa410871d03504cc426a901db149984
AysuMustafazade <mustafazadays16@gmail.com>
2026-01-21 11:10:52 +0000

    reads a text file and prints it

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,9 +9,9 @@
  * @letters: the number of letters it should read and print
  *
  * Return: the actual number of letters it could read and print
- *         if the file cannot be opened or read, return 0
- *         if filename is NULL return 0
- *         if write fails or does not write the expected amount of bytes, return 0
+ * if the file cannot be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
