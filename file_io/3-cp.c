86ff72cf5649542d167e27ac95c40331e4e6a96d
AysuMustafazade <mustafazadays16@gmail.com>
2026-01-21 12:17:31 +0000

    copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, exit with error codes otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, sizeof(buffer))) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			if (close(fd_from) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+			if (close(fd_to) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		if (close(fd_to) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
