0c9507fa6620a86e23b9c7f2a1c0cc837547a6ad
AysuMustafazade <mustafazadays16@gmail.com>
2026-01-21 12:54:23 +0000

    Prototypes

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -4,27 +4,27 @@
 #include <stdlib.h>
 
 /**
- * struct hash_node_s - Hash node
- * @key: key
- * @value: value
- * @next: pointer to next node
+ * struct hash_node_s - Node of a hash table
+ * @key: The key, string
+ * @value: The value corresponding to a key
+ * @next: Pointer to the next node
  */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
- * struct hash_table_s - Hash table
- * @size: size of array
- * @array: array of pointers to hash_node_t
+ * struct hash_table_s - Hash table structure
+ * @size: Size of the array
+ * @array: Array of pointers to the first node
  */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
